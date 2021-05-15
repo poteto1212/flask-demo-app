6d9af1682128869bc1ffa7385166a49218d4c840
Your Name <you@example.com>
2021-05-15 16:03:19 +0000

    init

--- main/config.py
+++ main/config.py
@@ -3,6 +3,6 @@ import os
 
 #右辺はherokuデータベースを使用
 #左辺はローカル開発時のデータベース
-SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL') or "sqlite:///test.db"
+SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL?sslmode=require') or "sqlite:///test.db"
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SECRET_KEY="secret key"
